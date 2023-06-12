37dbb5f7b9ece8f602c9b4ba9b3219dc8cff665c
Karan7zler <23karan2gmail.com>
2023-06-12 13:01:40 +0530

    Codes2

--- src/org/company/CompanyInfo.java
+++ src/org/company/CompanyInfo.java
@@ -10,6 +10,10 @@ public class CompanyInfo {
 		    System.out.println("Company Id: "+id);
 			}
 		
+		private void employeeName(String name) {
+			System.out.println("Employee Name: "+name);
+		}
+		
 		private void companyName(int accNum,long reg) {
 		    System.out.println("Employee Acc.Number: "+accNum+"\nCompany Reg.Number: "+reg);
 			}
@@ -19,6 +23,7 @@ public class CompanyInfo {
 			CompanyInfo c = new CompanyInfo();
 			c.companyName();
 			c.companyName(1234);
+			c.employeeName("Karan");
 			c.companyName(1205, 4210345565l);
 			
 		}
